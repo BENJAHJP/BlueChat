b4572cf0b5eb70d5cede59a33e8a6ece3c58bd50
BENJAH <benjahjp@gmail.com>
2023-03-11 14:17:29 +0300

    first

--- app/src/main/java/com/example/bluechat/MainActivity.kt
+++ app/src/main/java/com/example/bluechat/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.bluechat
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,6 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bluechat.ui.theme.BlueChatTheme
@@ -27,6 +30,8 @@ class MainActivity : ComponentActivity() {
         bluetoothManager?.adapter
     }
 
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -40,6 +45,23 @@ class MainActivity : ComponentActivity() {
                     val canEnableBluetooth = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
                         perms[Manifest.permission.BLUETOOTH_CONNECT] == true
                     } else true
+
+                    if (canEnableBluetooth && !isBluetoothEnabled){
+                        enabledBluetoothLauncher.launch(
+                            Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                        )
+                    }
+                }
+                
+                LaunchedEffect(key1 = true){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                        permissionLauncher.launch(
+                            arrayOf(
+                                Manifest.permission.BLUETOOTH_CONNECT,
+                                Manifest.permission.BLUETOOTH_SCAN
+                            )
+                        )
+                    }
                 }
             }
         }
