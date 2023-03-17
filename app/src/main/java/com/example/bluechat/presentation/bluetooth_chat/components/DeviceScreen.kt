e966836a7f2058c1a6ed778c956bff0556cefaed
BENJAH <benjahjp@gmail.com>
2023-03-17 21:19:55 +0300

    fix

--- app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/DeviceScreen.kt
+++ app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/DeviceScreen.kt
@@ -12,10 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -60,6 +62,16 @@ fun DeviceScreen(
                 Button(onClick = { viewModel.waitForIncomingConnections() }) {
                     Text(text = "Start server")
                 }
+            }
+        }
+        if (state.isConnecting){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CircularProgressIndicator()
+                Text(text = "Connecting ...")
             }
         }
     }
