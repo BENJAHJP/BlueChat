2e9af0987db0127b5d8248f802b41e47abaf6c05
BENJAH <benjahjp@gmail.com>
2023-03-17 14:37:05 +0300

    fix

--- app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/ChatScreen.kt
+++ app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/ChatScreen.kt
@@ -1,6 +1,8 @@
 package com.example.bluechat.presentation.bluetooth_chat.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,10 +11,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -20,18 +26,17 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
-import com.example.bluechat.presentation.bluetooth_chat.BluetoothUiState
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.bluechat.presentation.bluetooth_chat.BluetoothViewModel
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ChatScreen(
-    state: BluetoothUiState,
-    onDisconnect: () -> Unit,
-    onSendMessage: (String) -> Unit
+    viewModel: BluetoothViewModel = hiltViewModel(),
 ) {
-    var message = rememberSaveable {
-        mutableStateOf("")
-    }
+    val state = viewModel.state.collectAsState()
+
+    var message = rememberSaveable { mutableStateOf("") }
 
     val keyboardController = LocalSoftwareKeyboardController.current
 
@@ -41,13 +46,14 @@ fun ChatScreen(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
                 text = "Messages",
                 modifier = Modifier.weight(1f)
             )
-            IconButton(onClick = { onDisconnect() }) {
+            IconButton(onClick = { viewModel.disconnectFromDevice() }) {
                 Icon(
                     imageVector = Icons.Default.Close,
                     contentDescription = "Disconnect"
@@ -57,9 +63,11 @@ fun ChatScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f)
+                .weight(1f),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ){
-            items(state.messages){ message ->
+            items(state.value.messages){ message ->
                 Column( modifier = Modifier.fillMaxWidth()) {
                     ChatMessage(
                         message = message,
@@ -68,6 +76,31 @@ fun ChatScreen(
                         )
                     )
                 }
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            TextField(
+                value = message.value,
+                onValueChange = { message.value = it},
+                modifier = Modifier.weight(1f),
+                placeholder = { Text(text = "Message")}
+            )
+            IconButton(
+                onClick = {
+                    viewModel.sendMessage(message.value)
+                    message.value = ""
+                    keyboardController?.hide()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Send,
+                    contentDescription = "Send message"
+                )
             }
         }
     }
