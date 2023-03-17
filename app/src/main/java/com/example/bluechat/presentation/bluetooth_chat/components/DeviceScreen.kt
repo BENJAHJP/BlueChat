7cd87ef8361ae571bc6145f725757a86c6bcaf9f
BENJAH <benjahjp@gmail.com>
2023-03-17 21:43:32 +0300

    fix

--- app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/DeviceScreen.kt
+++ app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/DeviceScreen.kt
@@ -3,10 +3,8 @@ package com.example.bluechat.presentation.bluetooth_chat.components
 import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
