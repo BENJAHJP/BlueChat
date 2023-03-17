0af0f8ecb960ba59f240b4d1e074591fe9948d01
BENJAH <benjahjp@gmail.com>
2023-03-17 11:26:14 +0300

    fix

--- app/src/main/java/com/example/bluechat/data/BluetoothMessageMapper.kt
+++ app/src/main/java/com/example/bluechat/data/BluetoothMessageMapper.kt
@@ -2,6 +2,16 @@ package com.example.bluechat.data
 
 import com.example.bluechat.domain.BluetoothMessage
 
+fun String.toBluetoothMessage(isFromLocalUser: Boolean): BluetoothMessage{
+    val name = substringBeforeLast("#")
+    val message = substringAfter("#")
+    return BluetoothMessage(
+        senderName = name,
+        message = message,
+        isFromLocalUser = isFromLocalUser
+    )
+}
+
 fun BluetoothMessage.toByteArray(): ByteArray {
     return "$senderName#$message".encodeToByteArray()
 }
