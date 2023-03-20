f75938990af075a6da21b6c5a9c10d420da25725
BENJAH <benjahjp@gmail.com>
2023-03-20 11:56:58 +0300

    fix

--- app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/WelcomeScreen.kt
+++ app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/WelcomeScreen.kt
@@ -1,14 +1,19 @@
 package com.example.bluechat.presentation.bluetooth_chat.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.bluechat.presentation.screens.Screens
 import com.example.bluechat.ui.theme.Black
@@ -20,9 +25,13 @@ fun WelcomeScreen(
 ) {
     Column(
         modifier = Modifier.background(color = Blue)
-            .fillMaxSize()
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Button(
+            modifier = Modifier.fillMaxWidth(),
             colors = ButtonDefaults.buttonColors(
                 containerColor = Black,
                 contentColor = Color.White
