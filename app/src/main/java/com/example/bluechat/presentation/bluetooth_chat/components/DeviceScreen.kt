eb4820508e690923307d54540a8e335750fc596f
BENJAH <benjahjp@gmail.com>
2023-03-20 11:58:14 +0300

    fix

--- app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/DeviceScreen.kt
+++ app/src/main/java/com/example/bluechat/presentation/bluetooth_chat/components/DeviceScreen.kt
@@ -72,6 +72,7 @@ fun DeviceScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
+                .padding(16.dp)
         )
         Row(
             modifier = Modifier.fillMaxWidth(),
